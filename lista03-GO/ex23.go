e784f87374325452e17a2ef00dd9c24935d5bb25
heitor-40 <heitorcosta@discente.ufg.br>
2026-05-03 20:52:03 -0300

    Criar ex23.go

---
+++ lista03-GO/ex23.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+
+    var s float64
+    sinal := 1.0
+    numerador := 1000.0
+
+    for i := 1; i <= n; i++ {
+        s += sinal * numerador / float64(i)
+        sinal *= -1
+        numerador -= 3
+    }
+    fmt.Printf("S = %.2f\n", s)
+}
