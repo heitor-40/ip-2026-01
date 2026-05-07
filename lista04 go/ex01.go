2e438e07a8b3c56e02f32c40afb00fe4a7ddbe64
heitor-40 <heitorcosta@discente.ufg.br>
2026-05-07 09:58:46 -0300

    Implement recursive power function in ex01.go

--- lista04 go/ex01.go
+++ lista04 go/ex01.go
@@ -1,1 +1,16 @@
 
+package main
+import "fmt"
+ func quad(x,n int)int{
+     if (n==0){
+         return 1
+     }
+     return x* quad(x,n-1)
+ }
+func main(){
+    var x, n int
+    
+    fmt.Scan(&x, &n)
+    
+    fmt.Printf("%d^%d:%d\n",x,n,quad(x,n))
+}
