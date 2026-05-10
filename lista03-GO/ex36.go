339f6a55891dfcdacf346f2339540621c59c8274
heitor-40 <heitorcosta@discente.ufg.br>
2026-05-10 00:45:59 -0300

    Atualizar o ex36.go

--- lista03-GO/ex36.go
+++ lista03-GO/ex36.go
@@ -1,1 +1,38 @@
 
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    if n == 0 {
+        fmt.Println("Hexadecimal: 0")
+        return
+    }
+
+    c := 0
+    t := n
+    for t > 0 {
+        c++
+        t /= 16
+    }
+
+    fmt.Print("Hexadecimal: ")
+    for i := c - 1; i >= 0; i-- {
+
+        pot:=1
+        for j:=0;j<i;j++ {
+            pot*=16
+        }
+
+        digito:=(n/pot)%16
+
+        if digito<10{
+            fmt.Print(digito)
+        }else{
+            fmt.Printf("%c", 'A'+digito-10)
+        }
+    }
+}
